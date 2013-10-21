4ce9743c34d231370ca883c25b6c65bbab508190
David Trail <napalm10@gmail.com>
2013-10-21 13:17:26 +0200

    Should probably allow for floats

--- send.php
+++ send.php
@@ -4,12 +4,12 @@ require_once "jsonrpcphp/includes/jsonRPCClient.php";
 $wdc = new jsonRPCClient("http://naypam:godlike@127.0.0.1:11082/");
 $ip = $_SERVER['REMOTE_ADDR'];
 $address = $_POST["address"];
-$amount = (int) $_POST["amount"];
+$amount = (float) $_POST["amount"];
 
 $validation_data = $wdc->validateaddress($address);
 
-if (!is_int($amount)) {
-	die("Amount entered wasn't an integer.");
+if (!is_float($amount)) {
+	die("Amount entered wasn't a number.");
 } elseif ($amount < 0) {
 	die("You can't withdraw an negative amount.");
 }
@@ -20,6 +20,8 @@ if ($wdc->getbalance($ip, 2) < $amount) {
 
 if (is_array($validation_data)) {
 	$wdc->sendfrom($ip, $address, $amount);
+	header('Location: index.php');
+	exit();
 } else {
 	die("The address you entered was not valid.");
 }
